f4ae09191c252eee0f920a1e456a60c7ae7a1c19
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-30 06:30:59 +0100

    Updated

--- src/BuscadorPeliculas.jsx
+++ src/BuscadorPeliculas.jsx
@@ -1,26 +1,26 @@
 import { useState } from "react";
 
 export const BuscadorPeliculas = () => {
+  const [busqueda, setbusqueda] = useState("");
 
-        const[busqueda, setbusqueda] = useState('')
+  const handleInputChange = (e) => {
+    setbusqueda(e.target.value);
+  };
 
-        const handleInputChange = (e) => {
-            setbusqueda(e.target.value)
-        }
-
-        return (
-            <div className="container">
-
-            <h1 className="title">Buscador de Películas</h1>
-            <form>
-                <input type="text"
-                placeholder="Escribir un película"
-                value={busqueda}
-                onChange={handleInputChange}
-                />
-                <button type="submit" className="search-button">Buscar</button>
-            </form>
-
-            </div>
-            );
-        }    
+  return (
+    <div className="container">
+      <h1 className="title">Buscador de Películas</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="Escribir un película"
+          value={busqueda}
+          onChange={handleInputChange}
+        />
+        <button type="submit" className="search-button">
+          Buscar
+        </button>
+      </form>
+    </div>
+  );
+};
