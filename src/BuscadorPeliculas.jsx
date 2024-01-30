8140d1cd83bd50e9381eef4fb682279e37877487
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-30 06:18:55 +0100

    Updated

--- src/BuscadorPeliculas.jsx
+++ src/BuscadorPeliculas.jsx
@@ -2,8 +2,8 @@ export const BuscadorPeliculas = () => {
         return (
             <div className="container">
 
-            <h1>Buscador de Películas</h1>
-            
+            <h1 className="title">Buscador de Películas</h1>
+
             </div>
             );
         }    
