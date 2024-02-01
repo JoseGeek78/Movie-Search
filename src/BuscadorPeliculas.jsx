f6ff72dbf02edeec119f795c77ac8f21dfb574a3
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-02-01 06:16:12 +0100

    Updated

--- src/BuscadorPeliculas.jsx
+++ src/BuscadorPeliculas.jsx
@@ -10,7 +10,7 @@ export const BuscadorPeliculas = () => {
   return (
     <div className="container">
       <h1 className="title">Buscador de Películas</h1>
-      <form>
+      <form onSubmit={}>
         <input
           type="text"
           placeholder="Escribir un película"
