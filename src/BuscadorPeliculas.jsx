c46eca3946d9997211872e49899a1a287cea6491
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-02-04 10:08:07 +0100

    Updated

--- src/BuscadorPeliculas.jsx
+++ src/BuscadorPeliculas.jsx
@@ -1,23 +1,22 @@
-import { useState } from "react";
+import { useState } from "react"
 
 export const BuscadorPeliculas = () => {
 
   const urlBase = 'https://api.themoviedb.org/3/search/movie'
   const API_KEY = 'acf5472e45e436d5026bfd4c5e792981'
 
-  const [busqueda, setBusqueda] = useState("");
-  const [peliculas, setPeliculas] = useState([]);
+  const [busqueda, setBusqueda] = useState('')
+  const [peliculas, setPeliculas] = useState([])
 
   const handleInputChange = (e) => {
-    setbusqueda(e.target.value);
-  };
+    setBusqueda(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetchPeliculas()
+  }
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  fetchPeliculas();
-};
-
-const fetchPeliculas = async () => {
+  const fetchPeliculas = async () => {
     try {
       const response = await fetch(`${urlBase}?query=${busqueda}&api_key=${API_KEY}`)
       const data = await response.json()
@@ -30,18 +29,31 @@ const fetchPeliculas = async () => {
 
   return (
     <div className="container">
+
       <h1 className="title">Buscador de Películas</h1>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
-          placeholder="Escribir un película"
+          placeholder="Escribí una película"
           value={busqueda}
           onChange={handleInputChange}
         />
-        <button type="submit" className="search-button">
-          Buscar
-        </button>
+        <button type="submit" className="search-button">Buscar</button>
       </form>
+
+
+      <div className="movie-list">
+        {peliculas.map((pelicula) => (
+          <div key={pelicula.id} className="movie-card">
+            <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`} alt={pelicula.title} />
+            <h2>{pelicula.title}</h2>
+            <p>{pelicula.overview}</p>
+          </div>
+
+        ))}
+
+      </div>
+
     </div>
-  );
-};
+  )
+}
